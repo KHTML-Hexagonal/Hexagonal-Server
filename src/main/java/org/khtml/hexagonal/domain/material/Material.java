b0b263b1942c2b698724de993ed610b251f66056
Park Sejin <jinpark99@naver.com>
2024-08-13 20:17:55 +0900

    [FEAT]: material api 구현

---
+++ src/main/java/org/khtml/hexagonal/domain/material/Material.java
@@ -0,0 +1,30 @@
+package org.khtml.hexagonal.domain.material;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.khtml.hexagonal.domain.building.Building;
+import org.khtml.hexagonal.domain.common.BaseEntity;
+
+@Table(name = "material")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Material extends BaseEntity {
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "building_id")
+    private Building building;
+
+    @Builder
+    public Material(String name, Building building) {
+        this.name = name;
+        this.building = building;
+    }
+
+}
